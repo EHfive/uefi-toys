b4a775ec94b82dc65230493fb4217e861a9c66c3
Huang-Huang Bao <eh5@sokka.cn>
2023-09-13 13:13:21 +0800

    loopdrv: improve build script to only add /subsystem link-arg on uefi

--- loopdrv/build.rs
+++ loopdrv/build.rs
@@ -1,4 +1,10 @@
+use std::env::var;
+
 fn main() {
-    println!("cargo:rustc-link-arg=/subsystem:EFI_BOOT_SERVICE_DRIVER");
+    let target = var("TARGET").unwrap();
+    if target.contains("uefi") {
+        println!("cargo:rustc-link-arg=/subsystem:EFI_BOOT_SERVICE_DRIVER");
+    }
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=/null");
 }
